4953f1609430ecbd6d5e406495ed0b9c63a1a896
Syriamme <roridaniel01@gmail.com>
2023-09-11 14:03:59 +0300

    incrementing a value

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -8,13 +8,16 @@ let myObject = {
 
 console.log(myObject);
 
-myObject.incr = () => {
-  myObject.value++;
+myObject.incr = function () 
+{
+  this.value++;
 };
 
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
+
 myObject.incr();
 console.log(myObject);
