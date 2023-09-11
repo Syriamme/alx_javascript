417aa81604b09b08b2fa05bac525596d51410022
Syriamme <roridaniel01@gmail.com>
2023-09-11 13:45:02 +0300

    execute x times a function

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 // a function that executes x times a function.
 
-exports.callMeMoby = function (x, theFunction) 
-{
-    for (let i = 0; i < x; i++) 
-    theFunction();
-};
+exports.callMeMoby = function (x, theFunction) {
+    let i = 0;
+    while (i < x) {
+      theFunction();
+      i++;
+    }
+  };
+  
