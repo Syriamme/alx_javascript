39fc0534d2c6e4d2e71b30c1471e2deeb5ca2a28
Syriamme <roridaniel01@gmail.com>
2023-09-15 13:41:36 +0300

    a class with arguments and conditional statement

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,14 +2,13 @@
 //a class rectangle with constructor that takes 2 arguments w and h
 
 class Rectangle {
-    constructor(w, h)
-    {
-        if (w > 0 && h > 0)
-        {
-            this.width = w;
-            this.height = h;
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 ) {
+            return {};
         }
+        this.width = w;
+        this.height = h;
     }
 }
+
 module.exports = Rectangle;
-  
