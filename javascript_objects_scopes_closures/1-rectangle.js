04c099f267f61aff9ea75b858ff1f1356dfe670a
Syriamme <roridaniel01@gmail.com>
2023-09-15 12:54:49 +0300

    a rectangle class with two arguments width and height

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,11 @@
+#!/usr/bin/node
+//a class rectangle with constructor that takes 2 arguments w and h
+
 class Rectangle {
     constructor(w, h) {
       this.width = w;
       this.height = h;
     }
   }
-  
+
+  module.exports = Rectangle;
