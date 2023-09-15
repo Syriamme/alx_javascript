7275e4c668881084958a99245b7df9ec7924d815
Syriamme <roridaniel01@gmail.com>
2023-09-16 01:24:47 +0300

    inheritance

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,26 +1,18 @@
 #!/usr/bin/node
 //a class Square that defines a square and inherits from Square of 5-square.js
 
-class Square {
-    constructor(size) {
-        this.size = size;
-    }
+const Sq = require('./5-square');
 
-    charPrint(c) {
-        if (c === undefined) {
-            c = 'X';
-        }
-
-        for (let i = 0; i < this.size; i++) {
-            console.log(c.repeat(this.size));
-        }
+class Square extends Sq {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
+  }
 }
 
-class InheritedSquare extends Square {
-    constructor(size) {
-        super(size);
-    }
-}
-
-module.exports = InheritedSquare;
+module.exports = Square;
