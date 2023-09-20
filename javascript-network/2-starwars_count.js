0d99d3069c92f6a60983240cfff1fafc601d77ce
Syriamme <roridaniel01@gmail.com>
2023-09-20 18:46:04 +0300

    a script that prints the number of movies where the character “Wedge Antilles” is present.

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,22 +1,45 @@
 #!/usr/bin/node
 // Script printing the number of movies with character ID 18
 
-#!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], (error, response, body) => {
-  if (!error) {
-    try {
-      const films = JSON.parse(body).results;
+async function countMoviesWithWedgeAntilles(apiUrl) {
+  try {
+    const response = await new Promise((resolve, reject) => {
+      request(apiUrl, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+
+    if (response.statusCode === 200) {
+      const data = JSON.parse(response.body);
+      const films = data.results;
       const characterId = 18;
+      let count = 0;
 
-      const moviesWithWedgeAntilles = films.filter((film) =>
-        film.characters.some((character) => character.endsWith(`/${characterId}/`))
-      );
+      for (const movie of films) {
+        if (movie.characters.some((character) => character.endsWith(`/${characterId}/`))) {
+          count++;
+        }
+      }
 
-      console.log(`Number of movies with Wedge Antilles: ${moviesWithWedgeAntilles.length}`);
-    } catch (parseError) {
-      console.error('Error parsing JSON:', parseError);
+      console.log(`Number of movies with Wedge Antilles: ${count}`);
+    } else {
+      console.error(`HTTP Status Code: ${response.statusCode}`);
     }
+  } catch (error) {
+    console.error('Error:', error);
   }
-});
+}
+
+if (process.argv.length < 3) {
+  console.error('Please provide the Star Wars API URL as an argument.');
+  process.exit(1);
+}
+
+const apiUrl = process.argv[2];
+countMoviesWithWedgeAntilles(apiUrl);
