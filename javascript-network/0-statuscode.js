492377fac3513c555a1d3ade51e5092460adae70
Syriamme <roridaniel01@gmail.com>
2023-09-20 17:29:30 +0300

    Javascript network - getting statucode

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -2,15 +2,15 @@
 // a script that display the status code of a GET request
 
 
-const req = require ('request');
+const req = require('request');
 
 const url = process.argv[2];
 
 request.get(url, (error, respone) => {
     if (error) {
-        console.error('Error:', error.message);
+        console.log (error);
         process.exit(1);
     }
     else
-    console.log('code: $response.statusCode')
+    console.log('code: + response.statusCode')
 });
