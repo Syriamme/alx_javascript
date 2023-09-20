a0ff79556d830ce142f9d5c6a6d45e231c9afdcc
Syriamme <roridaniel01@gmail.com>
2023-09-20 19:08:28 +0300

    a script that prints the number of movies where the character “Wedge Antilles” is present.

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,12 +1,19 @@
 #!/usr/bin/node
 // Script printing the number of movies with character ID 18
 
+
 const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node starwars_count.js <API_URL>');
+  process.exit(1);
+}
+
 const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
   if (error) {
-    console.log (error);
+    console.error('Error:', error);
     process.exit(1);
   }
 
@@ -14,12 +21,14 @@ request(apiUrl, (error, response, body) => {
     console.error('API request failed with status code:', response.statusCode);
     process.exit(1);
   }
-  const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+
   const filmsData = JSON.parse(body).results;
-  const mvWithWedge = filmsData.reduce((count, movie) => {
-    const withWedge = movie.characters.some((character) => character === characterUrl);
-    return withWedge ? count + 1 : count;
+  const characterUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+
+  const numberOfMoviesWithWedge = filmsData.reduce((count, movie) => {
+    const hasWedge = movie.characters.some((character) => character === characterUrl);
+    return hasWedge ? count + 1 : count;
   }, 0);
 
-  console.log(mvWithWedge);
+  console.log(numberOfMoviesWithWedge);
 });
