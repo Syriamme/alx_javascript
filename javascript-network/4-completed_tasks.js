bcb5a1bc3a116cf96d422f2f6e9ae502fc8628dc
Syriamme <roridaniel01@gmail.com>
2023-09-20 22:54:58 +0300

    a script that computes the number of tasks completed by user id.

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -2,41 +2,17 @@
 //a script that computes the number of tasks completed by user id
 
 const request = require('request');
-
-if (process.argv.length < 3) {
-  console.log('Please provide the API URL as an argument.');
-  process.exit(1);
-}
-
 request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.log('Error: Received status code', response.statusCode);
-    return;
-  }
-
-  const todos = JSON.parse(body);
-
-
-  const completed = {};
-
-
-  todos.forEach((todo) => {
-    if (todo.completed) {
-      if (!completed[todo.userId]) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
         completed[todo.userId] = 1;
-      } else {
-        completed[todo.userId]++;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
-    }
-  });
-
-
-  for (const userId in completed) {
-    console.log(`User ID ${userId}: ${completed[userId]} completed tasks`);
+    });
+    console.log(completed);
   }
 });
