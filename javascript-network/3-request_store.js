a58dab909bfced4cc0320f6c7e9e7963b9d722cd
Syriamme <roridaniel01@gmail.com>
2023-09-20 19:28:04 +0300

    a script that gets the contents of a webpage and stores it in a file.

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -3,21 +3,7 @@
 const request = require('request');
 const fs = require('fs');
 
+const filepath = process.argv[3];
 const url = process.argv[2];
-const filePath = process.argv[3];
 
-const writeStream = fs.createWriteStream(filePath);
-
-request(url)
-  .on('error', (error) => {
-    console.error('Error:', error);
-  })
-
-  .pipe(writeStream)
-  .on('error', (error) => {
-    console.error('Error writing to file:', error);
-  })
-
-  .on('finish', () => {
-    console.log(`Successfully saved the content of ${url} to ${filePath}`);
-  });
+request(url).pipe(fs.createWriteStream(filepath));
