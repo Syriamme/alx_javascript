c05434021735bd9bc52b6ab750bb4f9f7323a14e
Syriamme <roridaniel01@gmail.com>
2023-09-24 11:35:40 +0300

    lexical scoping and welcome message

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,13 @@
+// Lexical scoping and welcome message
+
+function welcome(firstName, lastName) {
+    let fullName = firstName + ' ' + lastName;
+  
+    function displayFullName() {
+      alert('Welcome ' + fullName + '!');
+    }
+  
+    displayFullName();
+  }
+
+  
