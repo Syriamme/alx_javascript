084bef91a193f407e4a93392551df44635bd4fef
Syriamme <roridaniel01@gmail.com>
2023-09-24 12:26:37 +0300

    Changing DOM with closure

--- Javascript_advanced/5-mode.js
+++ Javascript_advanced/5-mode.js
@@ -4,7 +4,7 @@
 
 function changeMode(size, weight, transform, background, color) {
     return function() {
-        document.body.style.fontSize = size + 'px';
+        document.body.style.fontSize = size;
         document.body.style.fontWeight = weight;
         document.body.style.textTransform = transform;
         document.body.style.backgroundColor = background;
