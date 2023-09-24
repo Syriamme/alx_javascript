c23ff3afed49887c889707b4a3a9d44823c529cc
Syriamme <roridaniel01@gmail.com>
2023-09-24 12:41:27 +0300

    Stack order and setTimeout

---
+++ Javascript_advanced/7-timeout.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//Stack order and setTimeout
+
+console.log("Start of the execution queue");
+
+setTimeout(function () {
+  console.log("Final code block to be executed using setTimeout (with delay of 0)");
+}, 0);
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+console.log("End of the loop printing");
